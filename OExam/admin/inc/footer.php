be70dcc13a36d9ebe5f1d67d828c37ed8d32e60a
sanket2000sonavane <sanket2000sonavane@gmail.com>
2021-03-03 19:07:52 +0530

    completed

---
+++ OExam/admin/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "Taikisha Engineering India PVT LTD"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
---
+++ OExam/inc/footer.php
@@ -0,0 +1,11 @@
+ <style>
+ .footeroption{background:#202633;color:#fff;text-align:center;padding:20px;font-family: "Times New Roman", Times, serif;}
+ </style>
+ 
+</section>
+<section class="footeroption">
+		<h2><?php echo " Welcome to Taikisha India PVT LTD"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
